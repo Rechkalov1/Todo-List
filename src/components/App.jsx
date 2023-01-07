5efb9b3c370c5c2a115cfac1eeb2544d5e82a7cc
EnVy <envy7321@gmail.com>
2023-01-07 14:50:42 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import ToDo from './todo/todo';
+import ToDoForm from './todoForm/todoForm';
 export const App = () => {
+  const [todos, setTodos] = useState([]);
+  const addTask = userInput => {
+    if (userInput) {
+      const newItem = {
+        id: nanoid(),
+        task: userInput,
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
+    }
+  };
+  const removeTask = id => {
+    setTodos([...todos.filter(todo => todo.id !== id)]);
+  };
+  const handleToggle = id => {
+    setTodos([
+      ...todos.map(todo =>
+        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
+      ),
+    ]);
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <header>
+        <h1>Список задач : {todos.length}</h1>
+      </header>
+      <ToDoForm addTask={addTask} />
+      {todos.map(todo => {
+        return (
+          <ToDo
+            todo={todo}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+            key={todo.id}
+          />
+        );
+      })}
     </div>
   );
 };
